aeee17ea4df7df4bdca6728eaee862dbd97752fb
Ryan Beatty <rvbeatty@email.wm.edu>
2023-06-25 18:25:17 -0400

    actually get building + linking working. can run the script now

--- solutions/src/main.c
+++ solutions/src/main.c
@@ -4,7 +4,48 @@
 #include <stdint.h>
 #include <stdio.h>
 
-int main() {
+#define STB_DS_IMPLEMENTATION
+#include "stb_ds.h"
+
+#define Assert(expr)      \
+    {                     \
+        if (!(expr)) {    \
+            breakpoint(); \
+            assert(0);    \
+        }                 \
+    }
+
+void __attribute__((noinline)) breakpoint() {}
+
+uint8_t* ReadFile(char* input_filename, size_t* buffer_length) {
+    FILE* fd = fopen(input_filename, "r");
+    fseek(fd, 0, SEEK_END);
+    long num_bytes = ftell(fd);
+    Assert(num_bytes != -1);
+    uint8_t* buffer = (uint8_t*)calloc(num_bytes, sizeof(uint8_t));
+    rewind(fd);
+    fread(buffer, num_bytes, sizeof(uint8_t), fd);
+    fclose(fd);
+    *buffer_length = num_bytes;
+    return buffer;
+}
+
+int main(int argc, char* argv[]) {
+    char* input_filename = NULL;
+    switch (argc) {
+        case 2: {
+            input_filename = argv[1];
+            break;
+        }
+        default: {
+            fprintf(stderr, "USAGE: ./main <input_filename>\n");
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    size_t buffer_length = 0;
+    uint8_t* disassembly = ReadFile(input_filename, &buffer_length);
+
     uint32_t Version = Sim86_GetVersion();
     printf("Sim86 Version: %u (expected %u)\n", Version, SIM86_VERSION);
     if (Version != SIM86_VERSION) {
@@ -17,9 +58,9 @@ int main() {
     printf("8086 Instruction Instruction Encoding Count: %u\n", Table.EncodingCount);
 
     uint32_t Offset = 0;
-    while (Offset < sizeof(ExampleDisassembly)) {
+    while (Offset < sizeof(disassembly)) {
         instruction Decoded;
-        Sim86_Decode8086Instruction(sizeof(ExampleDisassembly) - Offset, ExampleDisassembly + Offset, &Decoded);
+        Sim86_Decode8086Instruction(sizeof(disassembly) - Offset, disassembly + Offset, &Decoded);
         if (Decoded.Op) {
             Offset += Decoded.Size;
             printf("Size:%u Op:%s Flags:0x%x\n", Decoded.Size, Sim86_MnemonicFromOperationType(Decoded.Op),
