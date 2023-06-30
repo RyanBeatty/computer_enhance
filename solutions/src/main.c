4603abe74d767952aeb9228afd03c399bb8ee6c4
Ryan Beatty <rvbeatty@email.wm.edu>
2023-06-30 19:51:01 -0400

    get tests up to 39 working

--- solutions/src/main.c
+++ solutions/src/main.c
@@ -1,6 +1,7 @@
 #include "sim86_shared.h"
 // hello world
 
+#include <inttypes.h>
 #include <stdint.h>
 #include <stdio.h>
 
@@ -30,6 +31,65 @@ uint8_t* ReadFile(char* input_filename, size_t* buffer_length) {
     return buffer;
 }
 
+void PrintRegisterAccess(register_access reg, FILE* stream) {
+    // TODO: I think both of these are for the internals of the decoder. Seemed to mostly be used when convertint to the
+    // reg name. Assert(!reg.Count); Assert(!reg.Offset);
+    const char* reg_name = Sim86_RegisterNameFromOperand(&reg);
+    fprintf(stream, "%s", reg_name);
+}
+
+void PrintMemoryAccess(effective_address_expression address, FILE* stream) {
+    fprintf(stream, "[");
+    PrintRegisterAccess(address.Terms[0].Register, stream);
+    if (address.Terms[1].Register.Index) {
+        fprintf(stream, " + ");
+        PrintRegisterAccess(address.Terms[1].Register, stream);
+    }
+    if (address.Displacement) {
+        const char sign = address.Displacement >= 0 ? '+' : '-';
+        fprintf(stream, " %c ", sign);
+        fprintf(stream, "%" PRId32 "", address.Displacement);
+    }
+    fprintf(stream, "]");
+}
+
+void PrintOperand(instruction_operand operand, FILE* stream) {
+    switch (operand.Type) {
+        case Operand_None: {
+            break;
+        }
+        case Operand_Register: {
+            PrintRegisterAccess(operand.Register, stream);
+            break;
+        }
+        case Operand_Memory: {
+            PrintMemoryAccess(operand.Address, stream);
+            break;
+        }
+        case Operand_Immediate: {
+            immediate imm = operand.Immediate;
+            Assert(imm.Flags == 0);
+            fprintf(stream, "%" PRId32 "", imm.Value);
+            break;
+        }
+        default: {
+            fprintf(stderr, "Unknown or None operand type: %d\n", operand.Type);
+            exit(EXIT_FAILURE);
+        }
+    }
+}
+
+void PrintInstruction(instruction instruction, FILE* stream) {
+    const char* op = Sim86_MnemonicFromOperationType(instruction.Op);
+    fprintf(stream, "%s ", op);
+    PrintOperand(instruction.Operands[0], stream);
+    if (instruction.Operands[1].Type) {
+        fprintf(stream, ", ");
+    }
+    PrintOperand(instruction.Operands[1], stream);
+    fprintf(stream, "\n");
+}
+
 int main(int argc, char* argv[]) {
     char* input_filename = NULL;
     switch (argc) {
@@ -47,7 +107,7 @@ int main(int argc, char* argv[]) {
     uint8_t* disassembly = ReadFile(input_filename, &buffer_length);
 
     uint32_t Version = Sim86_GetVersion();
-    printf("Sim86 Version: %u (expected %u)\n", Version, SIM86_VERSION);
+    printf("; Sim86 Version: %u (expected %u)\n", Version, SIM86_VERSION);
     if (Version != SIM86_VERSION) {
         printf("ERROR: Header file version doesn't match DLL.\n");
         return -1;
@@ -55,16 +115,15 @@ int main(int argc, char* argv[]) {
 
     instruction_table Table;
     Sim86_Get8086InstructionTable(&Table);
-    printf("8086 Instruction Instruction Encoding Count: %u\n", Table.EncodingCount);
+    printf("; 8086 Instruction Instruction Encoding Count: %u\n", Table.EncodingCount);
 
     uint32_t Offset = 0;
-    while (Offset < sizeof(disassembly)) {
+    while (Offset < buffer_length) {
         instruction Decoded;
-        Sim86_Decode8086Instruction(sizeof(disassembly) - Offset, disassembly + Offset, &Decoded);
+        Sim86_Decode8086Instruction(buffer_length - Offset, disassembly + Offset, &Decoded);
         if (Decoded.Op) {
             Offset += Decoded.Size;
-            printf("Size:%u Op:%s Flags:0x%x\n", Decoded.Size, Sim86_MnemonicFromOperationType(Decoded.Op),
-                   Decoded.Flags);
+            PrintInstruction(Decoded, stdout);
         } else {
             printf("Unrecognized instruction\n");
             break;
--- solutions/test.sh
+++ solutions/test.sh
@@ -2,6 +2,15 @@
 set -Eeuo pipefail
 
 FILENAME=$(find ../perfaware/part1/ -not -name "*.asm" -not -name "*.txt" -not -name "*.cpp" -name "*$1*")
-./build/main $FILENAME > ./build/test.asm;
-nasm ./build/test.asm -o ./build/test
-diff ./build/test $FILENAME
+
+if [ ${2-default} == "default" ]
+then
+    # Test dissasembly.
+    ./build/main $FILENAME > ./build/test.asm;
+    nasm ./build/test.asm -o ./build/test
+    diff ./build/test $FILENAME
+else
+    # Run with gdb.
+    gdb -ex run --args ./build/main $FILENAME
+fi
+
